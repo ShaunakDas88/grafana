9b6dbd80dc57f0fea5847f88f198db691604d0fc
ShaunakDas88 <shaunakdas88@gmail.com>
2021-01-06 09:45:12 -0800

    initial work to get filtering of annotation by tags on each panel

--- public/app/features/annotations/annotations_srv.ts
+++ public/app/features/annotations/annotations_srv.ts
@@ -54,6 +54,76 @@ export class AnnotationsSrv {
         });
 
         annotations = dedupAnnotations(annotations);
+        const currentVariables = options.dashboard.templating.list;
+        const filteredAnnotations = [];
+        // iterate through prospective annotations
+        for (const annotation of annotations) {
+          let removeAnnotation = false;
+          // the case that this annotation contains tags
+          if (typeof annotation.tags !== 'undefined') {
+            for (const variable of currentVariables) {
+              removeAnnotation = true;
+              const variableName = variable.name;
+              let variableSelectedOptions = variable.current.value;
+              // handle the case that only a single option has been selected, store the value as a single element in a list
+              if (typeof variableSelectedOptions === 'string') {
+                variableSelectedOptions = [variable.current.value];
+              }
+              const relevantAnnotationFilters = options.dashboard.annotations.list.filter(a => a.name === variableName);
+              // no options are selected for this variable, we will not respect any filtering then
+              if (variableSelectedOptions.length === 0) {
+                removeAnnotation = false;
+              } else {
+                // the case we've selected all, need to modify variableSelectedOptions accordingly
+                if (variableSelectedOptions[0] === '$__all') {
+                  variableSelectedOptions = variable.options
+                    .map((o: any) => o.value)
+                    .filter((o: any) => o !== '$__all');
+                }
+                // there are annotation filters that leverage this variable
+                if (relevantAnnotationFilters.length > 0) {
+                  for (const relevantAnnotationFilter of relevantAnnotationFilters) {
+                    if (!relevantAnnotationFilter.filterByVariable) {
+                      removeAnnotation = false;
+                    } else {
+                      // the case that the annotation tags will need to respect this filter due to being enabled
+                      if (relevantAnnotationFilter.enable === true) {
+                        for (const selectedOption of variableSelectedOptions) {
+                          if (annotation.tags.includes(selectedOption)) {
+                            removeAnnotation = false;
+                          }
+                          // do not  check the other selected options for this variable
+                          if (removeAnnotation === false) {
+                            break;
+                          }
+                        }
+                      } else {
+                        removeAnnotation = false;
+                      }
+                      // annotation already respect one annotation filter that leverages current variable, no need to go
+                      // through the other annotation filters that leverage this currentVariables
+                      if (removeAnnotation === false) {
+                        break;
+                      }
+                    }
+                  }
+                } else {
+                  removeAnnotation = false;
+                }
+                // current annotation does not satisfy the current variable's filtering requirements, don't check the rest of the variables
+                if (removeAnnotation === true) {
+                  break;
+                }
+              }
+            }
+          } else {
+            removeAnnotation = true;
+          }
+          if (removeAnnotation === false) {
+            filteredAnnotations.push(annotation);
+          }
+        }
+        annotations = filteredAnnotations;
         annotations = makeRegions(annotations, options);
 
         // look for alert state for this panel
